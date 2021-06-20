64390a029fd9b82472aa06713c4b17410e59b43d
Rajeshwar <rajeshwar.r.shinde1091998@gmail.com>
2021-06-20 09:41:02 +0530

    message

---
+++ 17-6-21/timer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+int main(){
+
+    struct timespec tstart={0,0}, tend={0,0};
+    clock_gettime(CLOCK_MONOTONIC, &tstart);
+   
+	printf("Hey There");
+    clock_gettime(CLOCK_MONOTONIC, &tend);
+    printf("some_long_computation took about %.5f seconds\n",
+           ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) - 
+           ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec));
+
+return 0;
+}
+
